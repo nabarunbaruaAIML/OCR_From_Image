7bb7de202283d7df8bb70b7d3aef6cc3b05e8ef8
Nabarun Barua <64695833+nabarunbaruaAIML@users.noreply.github.com>
2022-05-26 16:22:05 +0530

    minnor change

--- OCR.py
+++ OCR.py
@@ -41,7 +41,6 @@ class NpEncoder(json.JSONEncoder):
 def save_json(path, data):
     with open(path, "w") as f:
         json.dump(data, f, indent=4,cls=NpEncoder)
-
     logging.info(f"json file saved at: {path}")
 
 def main():
@@ -54,21 +53,14 @@ def main():
     lst_ocr_txt = []
     lst_ocr_prob= []
     for key,file in enumerate(list_of_files):
-        # print(file)
-        src = join('Artifacts\memediff5k', file)
-        # print(src)
+        src = join('Artifacts\memediff5k', file) 
         ocr_result = ocr.readtext(src)
-        # lst_file.append(file)
-        # lst_ocr_result.append(ocr_result)
         for ocr_line in ocr_result:
             lst_file.append(file)
             lst_ocr_cordinates.append(ocr_line[0])
             lst_ocr_txt.append(ocr_line[1])
             lst_ocr_prob.append(ocr_line[2])
         progress_bar.update(1)
-        # if key ==5:
-        #     # print(lst_ocr_result)
-        #     break
             
     Dict_File ={}
     Dict_File['File'] = lst_file
@@ -78,21 +70,12 @@ def main():
     save_json('Artifacts/Five_Thousand_Meme_json.json', Dict_File)
     
 
-
-
 """
     Main Function Execution
 """
 if __name__ == '__main__':
-    # args = argparse.ArgumentParser()
-    # args.add_argument("--config", "-c", default="configs/config.yaml")
-    # args.add_argument("--params", "-p", default="params.yaml")
-    # # args.add_argument("--secret","-s", default="configs/secrets.yaml")
-    # parsed_args = args.parse_args()
-
     try:
         logging.info(f">>>>> {STAGE} started <<<<<")
-        # main(parsed_args)
         main()
         logging.info(f">>>>> {STAGE}  completed! <<<<<\n\n")
     except Exception as e:
